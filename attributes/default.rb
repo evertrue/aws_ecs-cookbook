82e5f949e71b3de256e322d8fe62b502f7fb03b7
Dan McGuane <daniel.mcguane@evertrue.com>
2018-10-04 15:18:09 -0400

    add Ecs api user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['aws_ecs']['cluster_name'] = 'test'
 default['aws_ecs']['data_dir'] = '/datadir'
 default['aws_ecs']['log_file'] = '/log/ecs-agent.log'
+default['aws_ecs']['aws_api_user'] = 'Ecs'
 
 default['storage']['ebs_volumes']['vol1'] = {
   'device' => '/dev/xvde',
