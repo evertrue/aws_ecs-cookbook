64857bae5da2c2bd5654575620c8d83b2b9a9bd4
Dan McGuane <daniel.mcguane@evertrue.com>
2018-10-04 13:57:34 -0400

    use ebs volume on ecs servers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,10 @@
 default['aws_ecs']['cluster_name'] = 'test'
 default['aws_ecs']['data_dir'] = '/datadir'
 default['aws_ecs']['log_file'] = '/log/ecs-agent.log'
+
+default['storage']['ebs_volumes']['vol1'] = {
+  'device' => '/dev/xvde',
+  'size' => 100,
+  'delete_on_termination' => false,
+  'volume_type' => 'gp2'
+}
