627de69fad8e4e1574f954d9068c3cb604be3d2a
Dan McGuane <daniel.mcguane@evertrue.com>
2018-10-05 13:27:07 -0400

    override missing filesystem attrs with those from filesystem2

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,10 @@ default['aws_ecs']['data_dir'] = '/datadir'
 default['aws_ecs']['log_file'] = '/log/ecs-agent.log'
 default['aws_ecs']['aws_api_user'] = 'Ecs'
 
+default['filesystem']['by_mountpoint'] = node['filesystem2']['by_mountpoint']
+default['filesystem']['by_device'] = node['filesystem2']['by_device']
+default['filesystem']['by_pair'] = node['filesystem2']['by_pair']
+
 default['storage']['ebs_volumes']['vol1'] = {
   'device' => '/dev/xvde',
   'size' => 100,
